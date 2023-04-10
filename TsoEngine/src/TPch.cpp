7292babb071543527428c69b00caba25b325c503
MoChuan <zuosicheng@xiaohongshu.com>
2023-04-10 17:40:42 +0800

    pchheader

---
+++ TsoEngine/src/TPch.cpp
@@ -0,0 +1,8 @@
+//
+//  TPch.cpp
+//  TsoEngine
+//
+//  Created by user on 2023/4/10.
+//
+
+#include "TPch.h"
---
+++ TsoEngine/src/TPch.h
@@ -0,0 +1,24 @@
+//
+//  TPch.hpp
+//  TsoEngine
+//
+//  Created by user on 2023/4/10.
+//
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <stdio.h>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#ifdef TSO_PLATFORM_WINDOWS
+    #include <Windows.h>
+#endif
