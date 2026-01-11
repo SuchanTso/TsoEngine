be12db4a1474ba9e7639b86cbf06b101fc061d87
suchantso_lab <1329495987@qq.com>
2026-01-11 15:16:45 +0800

    (feat)Text: support text material rendering

--- TsoEngine/src/Tso/Renderer/Renderer2DMaterial.h
+++ TsoEngine/src/Tso/Renderer/Renderer2DMaterial.h
@@ -5,8 +5,11 @@
 #include "Tso/Renderer/Camera.h"
 #include "glm/glm.hpp"
 #include <vector>
+#include "Tso/Scene/Component.h"
 
 namespace Tso {
+class Font;
+
 struct MaterialInstanceComponent;
     // 命令结构体
     struct DrawQuadCommand {
@@ -19,7 +22,14 @@ struct MaterialInstanceComponent;
     };
 
     struct DrawStringCommand {
-        // ... 你的文字命令定义
+        Ref<Font> Font;
+        glm::mat4 Transform;
+        std::string Text;
+        glm::vec4 Color;
+        TextParam TextParam;
+        int EntityID;
+        Ref<Material> Material;
+        glm::vec4 CustumData;
     };
 
     class Renderer2DMaterial {
@@ -27,6 +37,7 @@ struct MaterialInstanceComponent;
         struct Statistics {
             uint32_t DrawCalls = 0;
             uint32_t QuadCount = 0;
+            uint32_t TextCount = 0;
             // ...
         };
 
@@ -41,6 +52,10 @@ struct MaterialInstanceComponent;
 
         // 提交命令
         static void DrawQuad(const glm::mat4& transform,const std::vector<glm::vec2>& texcoord , int entityID, Ref<Material> material,MaterialInstanceComponent* overrideComp);
+        
+        static void DrawString(const Ref<Font> font , const glm::mat4& transform , const std::string& text , const TextParam& textParam , const int& entityID, Ref<Material> material,MaterialInstanceComponent* overrideComp);
+
+
 //        static void DrawQuad(const glm::mat4& transform, const glm::vec4& color, int entityID, Ref<Material> material);
 //        static void DrawQuad(const glm::mat4& transform, Ref<Texture2D> texture, int entityID, Ref<Material> material);
 //        static void DrawQuad(const glm::mat4& transform, Ref<SubTexture2D> subTexture, int entityID, Ref<Material> material);
@@ -50,7 +65,15 @@ struct MaterialInstanceComponent;
         static void ResetStats();
 
     private:
+        template<typename T>
+        static void InitRenderer(Ref<VertexArray>& vertexArray, Ref<VertexBuffer>& vertexBuffer, T** vertexBase , Ref<IndexBuffer>& indexBuffer ,const BufferLayout& layout);
         static void StartBatch();
         static void RenderBatch(Ref<Material> material, Ref<Texture2D> overrideTexture, uint32_t count);
+        static void RenderQuadBatch(Ref<Material> material, uint32_t count);
+        static void RenderTextBatch(Ref<Material> material, uint32_t count);
+        static uint32_t GenerateTextVertices(const DrawStringCommand& cmd);
+
+        static void FlushQuad();
+        static void FlushString();
     };
 }
